d1690f9040c95d5d0b8650d7b1063768170c3d30
Adedeji Taiwo <dtaiwo45@gmail.com>
2022-11-25 16:54:56 +0100

    tasting notify

--- src/pages/RegistrationVerify.tsx
+++ src/pages/RegistrationVerify.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
 import Button from '../components/Button';
 import NotFound from './NotFound';
 import Preloader from '../components/Preloader';
@@ -7,9 +8,28 @@ import successGif from '../assets/registerVerify/check.gif';
 
 
 const RegistrationVerify = () => {
+  const [validUrl, setValidUrl] = useState<boolean>(false);
   const [delay, setDelay] = useState<boolean>(true);
+  const param = useParams();
   const navigate = useNavigate();
 
+
+
+  useEffect(() => {
+    const verifyEmailUrl = async () => {
+
+      const baseUrl = import.meta.env.VITE_HOST_BASE_URL;
+
+      try {
+        const url = `${baseUrl}/user/email-verify/${param.token}`;
+        await axios.get(url);
+        setValidUrl(true);
+      } catch (error) {
+        setValidUrl(false);
+      }
+    };
+    verifyEmailUrl();
+  }, [param])
 
   //delay loading components
   setTimeout(() => {
@@ -21,11 +41,11 @@ const RegistrationVerify = () => {
 
   const Success = () => (
     <div data-aos="fade-up" data-aos-once="true" data-aos-delay="500" className="flex items-center min-h-screen p-6 bg-gray-50">
-      <div className="flex-1 h-full p-8 max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl">
-        <div className="flex flex-col justify-center items-center text-center"> <div className="mx-auto flex items-center justify-center md:w-[200px] w-[200px]" style={{ backgroundColor: '#f5f5f9', borderRadius: '50%' }}>
+      <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl">
+        <div className="flex flex-col justify-center items-center text-center"> <div className="mx-auto flex items-center justify-center md:w-[250px] w-[200px]" style={{ backgroundColor: '#f1f1f5', borderRadius: '50%' }}>
           <img src={successGif} alt="success" className="w-full h-auto sm:max-w-[200px] max-w-[150px] mx-auto" />
         </div>
-          <div className="flex flex-col gap-2 my-6">
+          <div className="mt-2 px-7 py-3">
             <h3 className="sm:text-2xl text-xl text-[#252641] font-semibold">Successful!</h3>
             <p className="text-sm md:text-base text-gray-500">Account has been Successfully registered.</p>
           </div>
@@ -45,14 +65,17 @@ const RegistrationVerify = () => {
 
   return (
     <div>
-      {delay 
-      ?
+      {delay ?
         (<Preloader />)
-      :
-       (<Success />)
-       }
+        :
+        (validUrl ? (
+          <Success />
+        ) : (
+          <NotFound />
+        ))}
     </div>
   )
 }
 
 export default RegistrationVerify;
+
