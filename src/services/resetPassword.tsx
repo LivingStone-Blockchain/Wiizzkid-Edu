3e14d74dd61f75006b054b7593cece122efb4ddd
Adedeji Taiwo <dtaiwo45@gmail.com>
2022-12-10 10:30:33 +0100

    fixed patch update

--- src/services/resetPassword.tsx
+++ src/services/resetPassword.tsx
@@ -15,7 +15,7 @@ const baseUrl = import.meta.env.VITE_HOST_BASE_URL;
 
 
 const resetPassword = async (credential: credentialsType) => {
-    const response = await axios.post(`${baseUrl}/user/password-reset-complete/`, credential);
+    const response = await axios.patch(`${baseUrl}/user/password-reset-complete/`, credential);
     return response.data;
 }
 
