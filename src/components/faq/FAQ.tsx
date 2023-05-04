51c7109aa1b481800532816380b67218cef505d4
Adedeji Taiwo <dtaiwo45@gmail.com>
2023-05-04 12:05:49 +0100

    added faq

---
+++ src/components/faq/FAQ.tsx
@@ -0,0 +1,47 @@
+import React, { useRef, useState, FC } from 'react'
+import {FiChevronUp} from 'react-icons/fi'
+import { faqData } from './components/faqData'
+
+
+
+
+
+const FAQ = () => {
+  const [active, setActive] = useState<number | null>(null)
+  const contentSpace = useRef<HTMLDivElement>(null!)
+
+
+  const toggleAccordion = (id: number) => {
+    setActive(id === active ? null : id)
+    }
+
+
+  return (
+    <div className="mt-24">
+    <div data-aos="fade-up" className="text-center max-w-screen-lg mx-auto mb-8">
+      <h1 className="md:text-3xl text-2xl font-bold mb-4 text-navy">Wiizzkid <span className="text-tomato">FAQs</span></h1>
+      <p className="text-gray-500 md:text-base text-sm leading-relaxed">We answer some questions that are commonly asked in the Wiizzkid community.</p>
+    </div>
+    {faqData.map(({id, title, content}) => (
+      <div className="flex flex-col gap-4">
+      <button
+        className="py-6 box-border appearance-none cursor-pointer focus:outline-none flex items-center justify-between bg-navy rounded-xl lg:px-6 px-4"
+        onClick={() => toggleAccordion(id)}
+      >
+        <p className="inline-block font-semibold text-white text-left">{title}</p>
+        <p className='flex items-center justify-center p-2 bg-navyLight rounded-lg'><FiChevronUp className={`${active !== id ? 'transform duration-700 ease' : 'transform duration-700 ease rotate-180'} inline-block text-white font-bold`} /></p>
+      </button>
+      <div
+        ref={contentSpace}
+        style={{ maxHeight: active !== id ? '0px' : `${contentSpace?.current!.scrollHeight}px` }}
+        className={`overflow-auto transition-max-height duration-700 ease-in-out scrollbar-hide`}
+      >
+        <div className="text-gray-500 md:text-base text-sm leading-relaxed lg:px-6 px-4 pb-8">{content}</div>
+      </div>
+    </div>
+    ))}
+    </div>
+  )
+}
+
+export default FAQ;
---
+++ src/components/faq/components/faqData.tsx
@@ -0,0 +1,32 @@
+export const faqData = [
+    {
+        id: 1,
+        title: " What kind of educational games are available on the Wiizzkid platform?",
+        content: 'Wiizzkid offers educational games covering various subjects such as math, science, geography, and sport. "Timestable" is a game that tests and improves math skills through answering multiplication questions. These games are designed to be interactive and engaging, making learning enjoyable.',
+    },
+    {
+        id: 2,
+        title: 'How do I participate in activities on the Wiizzkid platform?',
+        content: "To participate in activities on the Wiizzkid platform, you need to create an account and obtain Stone Tokens. Once you have Stone Tokens, you can use them to play educational games, earn more tokens, and participate in various activities on the platform."
+    },
+    {
+        id: 3,
+        title: 'What is Stone Token?',
+        content: 'Stone Token is the native cryptocurrency used on the Wiizzkid platform. It is used as a form of payment for playing educational games on the platform and as a reward for participating in various activities on the platform.',
+    },
+    {
+        id: 4,
+        title: 'How do I obtain Stone Tokens?',
+        content: 'You can obtain Stone Tokens by exchanging Ethereum (ETH) for them on our cryptocurrency exchange. Once you have Stone Tokens, you can use them to play educational games on the Wiizzkid platform and earn more tokens.',
+    },
+    {
+        id: 5,
+        title: 'What can I do with Stone Tokens?',
+        content: 'You can use Stone Tokens to play educational games on the Wiizzkid platform, earn more tokens, and participate in various activities on the platform. You can also exchange Stone Tokens for other cryptocurrencies or fiat currencies on supported cryptocurrency exchanges.',
+    },
+    {
+        id: 6,
+        title: 'Can I play the Wiizzkid games without the Stone Tokens?',
+        content: 'Yes, you can play the Wiizzkid games without using Stone Tokens. The platform offers a "London mode" option which allows users to play games in single-player mode without the need for tokens. This is a great option for users who want to try out the games and get a feel for the platform before committing to using their Stone Tokens.'
+    }
+]
