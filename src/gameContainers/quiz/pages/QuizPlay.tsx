6bfa315f3c5988fa1c9940c93f1f3b0cdefd2bce
Adedeji Taiwo <dtaiwo45@gmail.com>
2023-06-07 09:31:38 +0100

    fixed pop

--- src/gameContainers/quiz/pages/QuizPlay.tsx
+++ src/gameContainers/quiz/pages/QuizPlay.tsx
@@ -228,7 +228,7 @@ if (gameDetails?.game_mode === "london") {
               type='button'
               onClick={handleStartGame}
               disabled={loading}
-              className={`flex items-center justify-center ${loading ? 'sm:w-64 w-48' : 'sm:w-56 w-44'} text-white text-sm font-semibold px-5 py-3 ${loading ? "cursor-not-allowed bg-[#37385e]" : "cursor-pointer bg-navy"} ${playerTracker?.current_players === playerTracker?.total_players ? 'pointer-events-auto' : 'pointer-events-none'}`}
+              className={`flex items-center justify-center ${loading ? 'sm:w-64 w-48' : 'sm:w-56 w-44'} text-white text-sm font-semibold px-5 py-3 ${loading ? "cursor-not-allowed bg-[#37385e]" : "cursor-pointer bg-navy"}`}
               >
                 {loading && (<svg role="status" className="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
