d97fa4e624e5752a7bb7aab9e3170aec8fde60d3
Adedeji Taiwo <dtaiwo45@gmail.com>
2023-07-17 08:41:13 +0100

    added new categories

--- src/gameContainers/quiz/components/functions/categoryStringConveter.tsx
+++ src/gameContainers/quiz/components/functions/categoryStringConveter.tsx
@@ -3,25 +3,25 @@ const categoryStrings = (category: number) => {
     switch (category) {
         /*case 1:
             return "football";*/
-        case 2:
+        case 13:
             return "Arts & Literature";
-        case 3:
+        case 14:
             return "Film & TV";
-        case 4:
+        case 15:
             return "Food & Drink";
-        case 5:
-            return "general_knowledge";
-        case 6:
-            return "Geography";
-        case 7:
-            return  "History";
         case 8:
-            return  "Music";
-        case 9:
-            return "Science";
+            return "general_knowledge";
         case 10:
-            return "society_and_culture";
+            return "Geography";
+        case 16:
+            return  "History";
+        case 4:
+            return  "Music";
+        case 7:
+            return "Science";
         case 11:
+            return "society_and_culture";
+        case 9:
             return "sport_and_leisure";
         default:
             return "";
@@ -31,3 +31,37 @@ const categoryStrings = (category: number) => {
 
 
   export default categoryStrings;
+
+
+
+  /*
+  
+const categoryStrings = (category: number) => {
+    switch (category) {
+        case 1:
+            return "football";
+            case 2:
+                return "Arts & Literature";
+            case 3:
+                return "Film & TV";
+            case 4:
+                return "Food & Drink";
+            case 5:
+                return "general_knowledge";
+            case 6:
+                return "Geography";
+            case 7:
+                return  "History";
+            case 8:
+                return  "Music";
+            case 9:
+                return "Science";
+            case 10:
+                return "society_and_culture";
+            case 11:
+                return "sport_and_leisure";
+            default:
+                return "";
+        }
+      }
+  */
