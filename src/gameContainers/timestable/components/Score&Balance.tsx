6e86ef9530b9761ab550a8c22a3a52527c7af665
adedeji <adedeji.taiwo@smartchainlimited.com>
2023-08-08 21:09:41 +0100

    added timestable scoreboard

--- src/gameContainers/timestable/components/Score&Balance.tsx
+++ src/gameContainers/timestable/components/Score&Balance.tsx
@@ -3,6 +3,7 @@ import { TimestableContext, TimestableContextType } from '../../../context/times
 import { apiChartData } from '../../../components/dashboard/data/timeTableChartData';
 import { TokenContext, TokenContextType } from '../../../context/token.context';
 import { utils } from 'ethers';
+import { UserContext, UserContextType } from '../../../context/user.context';
 
 
 type BoardDataType = {
@@ -11,9 +12,10 @@ type BoardDataType = {
 }
 
 const ScoreBalance = () => {
-    const { timestableRecentGames } = useContext(TimestableContext) as TimestableContextType;
+    const { timestableRecentGames, user, score } = useContext(TimestableContext) as TimestableContextType;
     const { stBalance } = useContext(TokenContext) as TokenContextType;
-
+    const {  userDetail } = useContext(UserContext) as UserContextType;
+    
   
 
     
@@ -23,20 +25,41 @@ const ScoreBalance = () => {
   const scoreData =   dataPerYear?.map((item) => item.score);
   const latestScore = scoreData?.length > 0 ? scoreData[scoreData?.length - 1] : 0;
 
-    const boardData: BoardDataType[] = [
+  const userBoardData: BoardDataType[] = [
+    {
+      title: "Balance",
+      value: Number(utils.formatEther(stBalance)).toFixed(1),
+    },
+    {
+      title: "Winnings",
+      value: userDetail?.stone_token_winnings === undefined ? 0 : userDetail?.stone_token_winnings?.toFixed(1),
+    },
+    {
+      title: "Score",
+      value: latestScore,
+    }
+    ] 
+
+
+    const nonUserBoardData: BoardDataType[] = [
         {
           title: "Balance",
           value: Number(utils.formatEther(stBalance)).toFixed(1),
         },
         {
           title: "Score",
-          value: latestScore,
+          value: score,
         }
       ] 
 
+
+        //render board based on roles
+        const boardData = user ? userBoardData : nonUserBoardData;
+
+
   return (
     <div className='absolute md:bottom-5 bottom-[14px] lg:right-16 right-4 mx-auto text-white flex gap-4 items-center justify-center md:text-base text-sm'> 
-           <div className={`bg-gray-100 border-4 text-center flex w-[120px] md:w-[160px] p-1 md:p-2 items-center justify-center shadow-lg rounded-md border-tealLight`}>
+            <div className={`bg-gray-100 border-4 text-center flex ${user ? 'w-[170px] md:w-[240px]' : 'w-[120px] md:w-[160px]'} p-1 md:p-2 items-center justify-center shadow-lg rounded-md border-tealLight`}>
             {boardData.map((item) => (
               <div className="col-4" key={item.title}>
               <div className={`text-tealLight md:text-base text-sm bg-gray-200 md:p-2 p-[2px] font-semibold border-x border-gray-300`}>
