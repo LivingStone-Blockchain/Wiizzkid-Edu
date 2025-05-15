ce1fd8aea7f9fc2945a8ec0e07934b26473c1673
Richard-Famojuro <142928782+Richard-Famojuro@users.noreply.github.com>
2025-05-15 09:57:45 +0100

    Update for ARFeaturePage.tsx

--- src/pages/ARFeaturePage.tsx
+++ src/pages/ARFeaturePage.tsx
@@ -96,7 +96,6 @@ const ARFeaturePage: React.FC = () => {
             ))}
           </div>
 
-          
           {/* Watch Video button */}
           <button
             onClick={() => setShowVideo(true)}
